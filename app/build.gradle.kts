331f6ed6e739b960a55ad0faa4786d810a6d567f
ZeusDeev <zeus.cach@outlook.com>
2024-01-09 14:49:51 -0500

    Retrofit y clases modelo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,8 +64,13 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
+    // Hilt
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
+
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.8.1")
+    implementation ("com.squareup.retrofit2:converter-gson:2.8.1")
 
 
     testImplementation("junit:junit:4.13.2")
